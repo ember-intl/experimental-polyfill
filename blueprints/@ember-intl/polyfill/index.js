3c866426ecf1c465f4395ad5eace2811b48a3a12
Jason Mitchell <jasonmit@nestlabs.com>
2017-08-21 12:39:20 -0700

    Removing invalid license header

--- blueprints/@ember-intl/polyfill/index.js
+++ blueprints/@ember-intl/polyfill/index.js
@@ -1,11 +1,6 @@
 /* eslint-env node */
 'use strict';
 
-/**
- * Copyright 2015, Yahoo! Inc.
- * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
- */
-
 module.exports = {
   description: 'Create a new boilerplate configuration file',
 
